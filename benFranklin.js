0429b03d1c4d78ea04d0d70e0392ba1e80eaa15b
SteveMustanski <steve.mustanski@gmail.com>
2019-04-22 16:57:10 -0400

    split out person object to a separate module

--- benFranklin.js
+++ benFranklin.js
@@ -1,16 +1,14 @@
-const EventEmitter = require('events').EventEmitter;
-const util = require('util');
-
-const Person = function(name) {
-  this.name = name;
-};
-
-util.inherits(Person, EventEmitter);
+const Person = require('./lib/Person');
 
 let ben = new Person('Ben Franklin');
+let george = new Person('George Washington');
 
 ben.on('speak', function(said) {
   console.log(`${this.name}: ${said}`);
 });
-
 ben.emit('speak', 'Please do not name anything after me');
+
+george.on('speak', function(said) {
+  console.log(`${this.name}: ${said}`);
+});
+george.emit('speak', 'Best president evah!');
