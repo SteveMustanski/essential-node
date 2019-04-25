a8e4ec24356c8f8abe5cd4a9996516ffcb06f946
SteveMustanski <steve.mustanski@gmail.com>
2019-04-25 13:41:09 -0400

    added dictionary-api route and setup for cors.  changed index title to vt dictionary

--- express-server/app.js
+++ express-server/app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const port = 3000;
+
+let vtTerms = [
+  {
+    term: 'bong sau',
+    defined: 'wing arm',
+  },
+  {
+    term: 'tan sau',
+    defined: 'receiving hand',
+  },
+  {
+    term: 'Sil Lim Tao',
+    defined: 'Little Idea.  The first form.',
+  },
+];
 
 app.use((req, res, next) => {
   console.log(`${req.method} request for ${req.url}`);
@@ -9,6 +25,11 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static('./public'));
+app.use(cors());
+
+app.get('/dictionary-api', (req, res) => {
+  res.json(vtTerms);
+});
 
 app.listen(3000);
 
