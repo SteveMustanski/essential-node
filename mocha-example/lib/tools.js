6c76a68c17d734df39bb6f56242ec5a3e49d0d76
SteveMustanski <steve.mustanski@gmail.com>
2019-04-26 13:12:36 -0400

    added example of async test

--- mocha-example/lib/tools.js
+++ mocha-example/lib/tools.js
@@ -1,5 +1,22 @@
+const https = require('https');
+
 module.exports = {
   printName(person) {
     return `${person.last}, ${person.firstName}`;
+  },
+
+  loadWiki(person, callback) {
+    let url = `https://en.wikipedia.org/wiki/${person.first}_${person.last}`;
+
+    https.get(url, res => {
+      let body = '';
+      res.setEncoding('UTF-8');
+      res.on('data', chunk => {
+        body += chunk;
+      });
+      res.on('end', () => {
+        callback(body);
+      });
+    });
   },
 };
--- mocha-example/test/tools-spec.js
+++ mocha-example/test/tools-spec.js
@@ -1,9 +1,22 @@
 const expect = require('chai').expect;
 const tools = require('../lib/tools');
 
-describe('printName', () => {
-  it('should print the last name first', () => {
-    let results = tools.printName({ firstName: 'Steve', last: 'Mustanski' });
-    expect(results).to.equal('Mustanski, Steve');
+describe('tools', () => {
+  describe('printName', () => {
+    it('should print the last name first', () => {
+      let results = tools.printName({ firstName: 'Steve', last: 'Mustanski' });
+      expect(results).to.equal('Mustanski, Steve');
+    });
+  });
+
+  describe('loadWiki', function() {
+    this.timeout(5000);
+
+    it("should load Abraham Lincoln's wikipedia page", done => {
+      tools.loadWiki({ first: 'Abraham', last: 'Lincoln' }, html => {
+        expect(html).to.be.ok;
+        done();
+      });
+    });
   });
 });
