877953fff7917cfa724d9e00661a2998bf7fe47b
SteveMustanski <steve.mustanski@gmail.com>
2019-04-26 11:55:27 -0400

    added websocket example with draft chat application

---
+++ websockets/ws-client.js
@@ -0,0 +1,28 @@
+const ws = new WebSocket('ws://localhost:3000');
+
+ws.onopen = () => {
+  setTitle('Connected to Cyber Chat');
+};
+
+ws.onclose = () => {
+  setTitle('Disconnected');
+};
+
+ws.onmessage = payload => {
+  printMessage(payload.data);
+};
+
+document.forms[0].onsubmit = function() {
+  var input = document.getElementById('message');
+  input.value = '';
+};
+
+function setTitle(title) {
+  document.querySelector('h1').innerHTML = title;
+}
+
+function printMessage(message) {
+  var p = document.createElement('p');
+  p.innerText = message;
+  document.querySelector('div.messages').appendChild(p);
+}
---
+++ websockets/ws.js
@@ -0,0 +1,8 @@
+// a websocket example
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', ws => {
+  ws.send('Welcome to cyber chat');
+});
