50c9b53ecdb36124167ab6d5b165dd464655c6b7
SteveMustanski <steve.mustanski@gmail.com>
2019-04-29 14:53:41 -0400

    added sinon spy on console log but removed as it removed test results logging to terminal

--- mocha-example/test/order-spec.js
+++ mocha-example/test/order-spec.js
@@ -1,8 +1,9 @@
 const expect = require('chai').expect;
 const rewire = require('rewire');
+const sinon = require('sinon');
 const order = rewire('../lib/order'); // require with rewire so we can inject test data later
 
-describe('Ordering Items', () => {
+describe('Ordering Items', function() {
   beforeEach(function() {
     this.testData = [
       { sku: 'AAA', qty: 10 },
@@ -11,8 +12,15 @@ describe('Ordering Items', () => {
     ];
     // using rewire to use test inventoryData rather than real data
     order.__set__('inventoryData', this.testData);
+    // set up sinon spy
+    this.console = {
+      log: sinon.spy(),
+    };
+    // set the order console to point to sinon spy using rewire
+    // order.__set__('console', this.console);
   });
-  it('order an item when there are enough in stock', done => {
+  it('order an item when there are enough in stock', function(done) {
+    let _this = this;
     order.orderItem('CCC', 3, () => {
       done();
     });
